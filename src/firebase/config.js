7a76f1e28141e4305bb486229eedd559497c46aa
Daniel Torrijos <danieltorrijos@Daniels-MacBook.local>
2020-07-02 13:05:32 -0600

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASWSwk6gkn4tWhIfqtp3vKQLB4nqTIhO4",
+    authDomain: "newsy-15e6a.firebaseapp.com",
+    databaseURL: "https://newsy-15e6a.firebaseio.com",
+    projectId: "newsy-15e6a",
+    storageBucket: "newsy-15e6a.appspot.com",
+    messagingSenderId: "491075407700",
+    appId: "1:491075407700:web:a86ef7fa8348bd41f4c2c1"
+};
+
+export default firebaseConfig;
